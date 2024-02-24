dda9eae122743ccd2763e84c5a704294dcf9350f
DeclanHarty <111760726+DeclanHarty@users.noreply.github.com>
2024-02-24 13:33:52 -0500

    Added Smoother Collision of Clowns

--- Assets/Scripts/ClownScripts/ClownBehavior.cs
+++ Assets/Scripts/ClownScripts/ClownBehavior.cs
@@ -11,7 +11,7 @@ public class ClownBehavior : MonoBehaviour
 
     [SerializeField] private float minDistanceToEnemy;
 
-    [SerializeField] private bool isAbleToAttack;
+    private bool isAbleToAttack;
     [SerializeField] private int attackDamage;
     [SerializeField] private float attackInterval;
     private EnemyCheck enemyCheck;
@@ -31,12 +31,88 @@ public class ClownBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        state.Update();
+        nearestEnemy = enemyCheck.CheckForNearestEnemy();
+        allyOpposite = allyCheck.FindAllyDirectionSumOpposite();
+
+        // Flips Sprite depending on Velocity
+        if(rb.velocity == Vector2.zero){
+            return;
+        }else{
+            transform.localScale = new Vector2(Mathf.Abs(rb.velocity.x)/rb.velocity.x, transform.localScale.y);
+        }
     }
 
     void FixedUpdate(){
-        state.FixedUpdate();
+        if(nearestEnemy == null){
+            rb.velocity = Vector2.zero;
+            return;
+        }
+
+        Vector3 enemyPosition = nearestEnemy.transform.position;
+
+        if(Vector2.Distance(enemyPosition, transform.position) > 1.5f){
+            Vector2 directionToEnemy = (enemyPosition - transform.position);
+            directionToEnemy.Normalize();
+
+            if(allyOpposite == Vector2.zero){    
+                rb.velocity = directionToEnemy * speed;
+            }else{
+                rb.velocity += allyOpposite.normalized * bumpSpeed * Time.deltaTime;
+            } 
+            Debug.Log(rb.velocity);
+        }else if(isAbleToAttack == false){
+            rb.velocity = Vector2.zero;
+            isAbleToAttack = true;
+            Attack();
+        }else{
+            rb.velocity = Vector2.zero;
+        }
     }
+
+    void Attack(){
+        Debug.Log("Attack");
+        if(isAbleToAttack && nearestEnemy != null){
+            bool enemyIsDead = nearestEnemy.GetComponent<EnemyBehavior>().TakeDamage(attackDamage);
+            if(!enemyIsDead){
+                Invoke("Attack", attackInterval);
+            }else{
+                isAbleToAttack = false;
+            }
+        } 
+    }
+
+    public Rigidbody2D GetRigidbody(){
+        return rb;
+    }
+
+    public Transform GetTransform(){
+        return transform;
+    }
+
+    public EnemyCheck GetEnemyCheck(){
+        return enemyCheck;
+    }
+
+    public AllyCheck GetAllyCheck(){
+        return allyCheck;
+    }
+
+    public int GetAttackDamage(){
+        return attackDamage;
+    }
+
+    public float GetAttackInterval(){
+        return attackInterval;
+    }
+
+    public float GetSpeed(){
+        return speed;
+    }
+
+    public float GetBumpSpeed(){
+        return bumpSpeed;
+    }
+
 
     
 
